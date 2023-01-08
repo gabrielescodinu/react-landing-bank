29c48cf9778901ae3955029866284ef4dce94ed5
Gabriele S <web@pezzilli.com>
2023-01-09 00:39:36 +0100

    push

--- src/assets/index.js
+++ src/assets/index.js
@@ -23,6 +23,15 @@ import twitter from "./twitter.svg";
 import people01 from "./people01.png";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
+import planet01 from "./planet-01.png";
+import planet02 from "./planet-02.png";
+import planet03 from "./planet-03.png";
+import planet04 from "./planet-04.png";
+import planet05 from "./planet-05.png";
+import planet06 from "./planet-06.png";
+import planet07 from "./planet-07.png";
+import planet08 from "./planet-08.png";
+import planet09 from "./planet-09.png";
 
 export {
   airbnb,
@@ -50,4 +59,13 @@ export {
   people01,
   people02,
   people03,
+  planet01,
+  planet02,
+  planet03,
+  planet04,
+  planet05,
+  planet06,
+  planet07,
+  planet08,
+  planet09,
 };
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import {useState} from 'react';
+
+import { planet01, planet02, planet03, planet04, planet05, planet06, planet07, planet08, planet09, } from '../assets'
+
 
 const Stats = () => {
+  const [activeElementId, setActiveElementId] = useState(null);
+
+  function handleElementClick(id) {
+    setActiveElementId(id);
+  }
+
   return (
-    <div>Stats</div>
+    <>
+      <div className='lg:flex p-8 lg:px-20 4xl:px-40'>
+        <div className={`${activeElementId === 1 ? 'lg:w-4/5' : 'lg:w-1/3'} lg:h-[600px] h-52 lg:mx-4 cursor-pointer rounded-2xl mb-8 lg:mb-0 w-full relative`} onClick={() => handleElementClick(1)} style={{backgroundImage: `url(${planet01})`, backgroundPosition: 'center', backgroundSize: 'cover' }} >
+          <p className={`${activeElementId === 1 ? 'hidden' : ''} text-4xl font-bold text-white absolute bottom-8 m-8 -rotate-90`}> Element <br /> One</p>
+          <p className={`${activeElementId === 1 ? 'block' : 'hidden'} text-4xl font-bold text-white absolute bottom-0 w-full bg-[#000000ab] p-8 rounded-b-2xl`}> Element <br /> One</p>
+        </div>
+        <div className={`${activeElementId === 2 ? 'lg:w-4/5' : 'lg:w-1/3'} lg:h-[600px] h-52 lg:mx-4 cursor-pointer rounded-2xl mb-8 lg:mb-0 w-full relative`} onClick={() => handleElementClick(2)} style={{backgroundImage: `url(${planet02})`, backgroundPosition: 'center', backgroundSize: 'cover' }} >
+          <p className={`${activeElementId === 2 ? 'hidden' : ''} text-4xl font-bold text-white absolute bottom-8 m-8 -rotate-90`}> Element <br /> Two</p>
+          <p className={`${activeElementId === 2 ? 'block' : 'hidden'} text-4xl font-bold text-white absolute bottom-0 w-full bg-[#000000ab] p-8 rounded-b-2xl`}> Element <br /> Two</p>
+        </div>
+        <div className={`${activeElementId === 3 ? 'lg:w-4/5' : 'lg:w-1/3'} lg:h-[600px] h-52 lg:mx-4 cursor-pointer rounded-2xl mb-8 lg:mb-0 w-full relative`} onClick={() => handleElementClick(3)} style={{backgroundImage: `url(${planet03})`, backgroundPosition: 'center', backgroundSize: 'cover' }} >
+          <p className={`${activeElementId === 3 ? 'hidden' : ''} text-4xl font-bold text-white absolute bottom-8 m-8 -rotate-90`}> Element <br /> Three</p>
+          <p className={`${activeElementId === 3 ? 'block' : 'hidden'} text-4xl font-bold text-white absolute bottom-0 w-full bg-[#000000ab] p-8 rounded-b-2xl`}> Element <br /> Three</p>
+        </div>
+        <div className={`${activeElementId === 4 ? 'lg:w-4/5' : 'lg:w-1/3'} lg:h-[600px] h-52 lg:mx-4 cursor-pointer rounded-2xl mb-8 lg:mb-0 w-full relative`} onClick={() => handleElementClick(4)} style={{backgroundImage: `url(${planet04})`, backgroundPosition: 'center', backgroundSize: 'cover' }} >
+          <p className={`${activeElementId === 4 ? 'hidden' : ''} text-4xl font-bold text-white absolute bottom-8 m-8 -rotate-90`}> Element <br /> Four</p>
+          <p className={`${activeElementId === 4 ? 'block' : 'hidden'} text-4xl font-bold text-white absolute bottom-0 w-full bg-[#000000ab] p-8 rounded-b-2xl`}> Element <br /> Four</p>
+        </div>
+        <div className={`${activeElementId === 5 ? 'lg:w-4/5' : 'lg:w-1/3'} lg:h-[600px] h-52 lg:mx-4 cursor-pointer rounded-2xl mb-8 lg:mb-0 w-full relative`} onClick={() => handleElementClick(5)} style={{backgroundImage: `url(${planet05})`, backgroundPosition: 'center', backgroundSize: 'cover' }} >
+          <p className={`${activeElementId === 5 ? 'hidden' : ''} text-4xl font-bold text-white absolute bottom-8 m-8 -rotate-90`}> Element <br /> Five</p>
+          <p className={`${activeElementId === 5 ? 'block' : 'hidden'} text-4xl font-bold text-white absolute bottom-0 w-full bg-[#000000ab] p-8 rounded-b-2xl`}> Element <br /> Five</p>
+        </div>
+      </div>
+    </>
   )
 }
 
